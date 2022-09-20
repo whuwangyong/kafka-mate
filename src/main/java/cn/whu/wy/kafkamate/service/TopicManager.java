cd9f056ba10c356bed5ef0b0a1dd8435e41e1026
whuwangyong <whuwangyong@gmail.com>
2022-09-20 20:53:32 +0800

    createFooTopic4TriggeringDelete无效。尝试用topicUuid进行删除，也无效

--- src/main/java/cn/whu/wy/kafkamate/service/TopicManager.java
+++ src/main/java/cn/whu/wy/kafkamate/service/TopicManager.java
@@ -1,13 +1,17 @@
 package cn.whu.wy.kafkamate.service;
 
 import cn.whu.wy.kafkamate.bean.TopicInfo;
+import org.apache.kafka.clients.admin.TopicListing;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 public interface TopicManager extends AdminClientAware {
 
-    Set<String> listTopics(boolean listInternal) throws ExecutionException, InterruptedException;
+    Set<String> listTopicsName(boolean listInternal) throws ExecutionException, InterruptedException;
+
+    Collection<TopicListing> listTopics(boolean listInternal) throws ExecutionException, InterruptedException;
 
     Object createTopics(Set<TopicInfo> topicInfos) throws ExecutionException, InterruptedException;
 
--- src/main/java/cn/whu/wy/kafkamate/service/TopicManagerImpl.java
+++ src/main/java/cn/whu/wy/kafkamate/service/TopicManagerImpl.java
@@ -3,12 +3,12 @@ package cn.whu.wy.kafkamate.service;
 import cn.whu.wy.kafkamate.bean.TopicInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.ListTopicsOptions;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.TopicCollection;
+import org.apache.kafka.common.Uuid;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -36,10 +36,17 @@ public class TopicManagerImpl implements TopicManager {
     }
 
     @Override
-    public Set<String> listTopics(boolean listInternal) throws ExecutionException, InterruptedException {
+    public Set<String> listTopicsName(boolean listInternal) throws ExecutionException, InterruptedException {
         ListTopicsResult listTopicsResult = adminClient.listTopics(new ListTopicsOptions().listInternal(listInternal));
         return listTopicsResult.names().get();
     }
+
+    @Override
+    public Collection<TopicListing> listTopics(boolean listInternal) throws ExecutionException, InterruptedException {
+        ListTopicsResult listTopicsResult = adminClient.listTopics(new ListTopicsOptions().listInternal(listInternal));
+        return listTopicsResult.listings().get();
+    }
+
 
     /**
      * 批量创建topics。过滤掉名字为空和已存在的topics
@@ -47,7 +54,7 @@ public class TopicManagerImpl implements TopicManager {
     @Override
     public Object createTopics(Set<TopicInfo> topicInfos) throws ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
-        Set<String> existTopics = listTopics(false);
+        Set<String> existTopics = listTopicsName(false);
         List<NewTopic> topicsToCreate = topicInfos.stream()
                 .filter(topicInfo -> !StringUtils.isBlank(topicInfo.getName())
                         && topicInfo.getNumPartitions() != 0
@@ -69,17 +76,18 @@ public class TopicManagerImpl implements TopicManager {
     @Override
     public Object deleteTopics(Set<String> topics) throws ExecutionException, InterruptedException {
         Object o = doDeleteTopics(topics);
-        createFooTopic4TriggeringDelete();
+//        createFooTopic4TriggeringDelete();
         return o;
     }
 
     private Object doDeleteTopics(Set<String> topics) throws ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
-        Set<String> existTopics = listTopics(false);
-        Set<String> topicsToDelete = topics.stream()
-                .filter(existTopics::contains)
-                .collect(Collectors.toSet());
-        adminClient.deleteTopics(topicsToDelete).all().get();
+        Collection<TopicListing> existTopics = listTopics(false);
+        List<Uuid> topicsToDelete = existTopics.stream()
+                .filter(t -> topics.contains(t.name()))
+                .map(TopicListing::topicId)
+                .collect(Collectors.toList());
+        adminClient.deleteTopics(TopicCollection.ofTopicIds(topicsToDelete)).all().get();
         long now = System.currentTimeMillis();
         String info = String.format("delete topics: input size=%d, actually deleted size=%d, use %d ms.",
                 topics.size(), topicsToDelete.size(), now - start);
@@ -89,8 +97,7 @@ public class TopicManagerImpl implements TopicManager {
 
     /**
      * 由于kafka删除topic不会立即生效，只是标记为删除。
-     * 当创建新的topic时，才会触发真正的删除操作。
-     * 该方法创建一个临时topic，然后将其标记为删除。
+     * 该方法创建一个临时topic，然后将其删除，试图快速触发kafka的删除机制
      */
     private void createFooTopic4TriggeringDelete() throws ExecutionException, InterruptedException {
         NewTopic foo = new NewTopic("foo_" + System.currentTimeMillis(), 1, (short) 1);
